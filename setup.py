88645095c615188e02b026a08c7c5127e710ce59
Ethan Chappel <EthanChappel@users.noreply.github.com>
2019-07-02 00:15:35 -0500

    Add barebones setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""Setup Alpyca for distribution."""
+import setuptools
+
+setuptools.setup(
+    name="Alpyca",
+    description="Python interface for ASCOM Alpaca.",
+    author="Ethan Chappel",
+    url="https://github.com/EthanChappel/Alpyca",
+    version="1.0.0a0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+)
